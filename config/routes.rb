612373f5749a11fed56b1e22e3ff855a8aafd1b9
Igor <igorbest@gmail.com>
2014-03-02 14:04:14 -0600

    test deployment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Test1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'index#index'
+  resources :novels
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
