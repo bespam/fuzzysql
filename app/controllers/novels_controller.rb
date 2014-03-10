02e5fc33ef406e2f4d89c3629a915565ff1000d7
Igor <igorbest@gmail.com>
2014-03-10 16:08:06 -0500

    redeployment

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -9,19 +9,29 @@ class NovelsController < ApplicationController
     found = Set.new
     query_columns = ['title','author','description']
     
-    @keywords = params[:q].strip.split(" ")
-    @keywords.each do |word|
-        concept_url = 'http://conceptnet5.media.mit.edu/data/5.2/assoc/list/en/'+word+'?limit=50&filter=/c/en'
-        response = RestClient.get concept_url
-        body = JSON.parse response.body
-        @terms = body["similar"]
-        #do for each similar term found in conceptnet
-        @terms.each do |term, score|
-            #check match in query columns
-            query_columns.each do |query|
-                condition = query+" ~* ?"
+    if params[:q] != nil
+        @search_string = params[:q]
+        keywords = @search_string.strip.gsub(","," ").split(" ")
+        keywords.each do |word|
+            concept_url = 'http://conceptnet5.media.mit.edu/data/5.2/assoc/list/en/'+word+'?limit=50&filter=/c/en'
+            response = RestClient.get concept_url
+            body = JSON.parse response.body
+            terms = body["similar"]
+            #terms = [["a/a/a/"+word,1]]
+            #do for each similar term found in conceptnet
+            terms.each do |term, score|
                 word = term.strip.split("/")[3].gsub("_"," ")
-                results = Novel.find(:all, :conditions => [condition,".[^a-z]"+word+"[^a-z]."])
+                #check match in query columns
+                query_columns = ['author','title','description']
+                # three types of regular expressions
+                regex1 = "'.[^a-z]"+word+"[^a-z].'"
+                condition1 = query_columns.join(" ~* "+regex1+" or ")+" ~* "+regex1
+                regex2 = "'^"+word+"[^a-z].'"
+                condition2 = query_columns.join(" ~* "+regex2+" or ")+" ~* "+regex2
+                regex3 = "'.[^a-z]"+word+"$'"
+                condition3 = query_columns.join(" ~* "+regex3+" or ")+" ~* "+regex3            
+                condition = condition1 + " or " + condition2 + " or " + condition3 
+                results = Novel.find(:all, :conditions => condition)
                 found.merge(results)
                 results.each do |novel|
                     if ranked.has_key?(novel.id)
@@ -32,13 +42,13 @@ class NovelsController < ApplicationController
                 end
             end
         end
-    end
-    #create a output array
-    @output = []
-    sorted = ranked.sort_by {|k,v| -v}
-    sorted.each do |id, score|
-        line = found.detect {|f| f["id"] == id}
-        @output.push([line,score])
+        #create a output array
+        @output = []
+        sorted = ranked.sort_by {|k,v| -v}
+        sorted.each do |id, score|
+            line = found.detect {|f| f["id"] == id}
+            @output.push([line,score])
+        end
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Test1::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'index#index'
+  root 'novels#search'
   resources :novels
 
   # Example of regular route:
